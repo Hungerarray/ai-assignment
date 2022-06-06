602035463f5da362ec94c2110ecd1fe87609a995
Ranjan Ojha <59282692+Hungerarray@users.noreply.github.com>
2022-06-06 19:12:58 +0545

    Initial Refactor greedy

--- greedy.cpp
+++ greedy.cpp
@@ -1,83 +1,94 @@
-#include "utility.h"
-#include <algorithm>
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
+#include <deque>
+#include <array>
 
-Vertex S{"S"};
-Vertex A{"A"};
-Vertex B{"B"};
-Vertex C{"C"};
-Vertex D{"D"};
-Vertex G{"G"};
-
-std::unordered_map<Vertex, uint32_t> heuristicCost{{S, 8}, {A, 5}, {B, 6},
-                                                   {C, 3}, {D, 2}, {G, 0}};
-
-Graph initGraph() {
-  Graph graph;
-  graph.addEdge(S, A, 2)
-      .addEdge(S, B, 3)
-      .addEdge(A, C, 2)
-      .addEdge(A, B, 3)
-      .addEdge(A, D, 1)
-      .addEdge(B, D, 4)
-      .addEdge(C, D, 4)
-      .addEdge(C, G, 3)
-      .addEdge(D, G, 2);
-
-  return graph;
+constexpr uint32_t VERTCOUNT = 7;
+enum Vertex { S, A, B, C, D, H, G };
+char VertexName(Vertex v) {
+  switch(v) {
+    case S:
+      return 'S';
+    case A:
+      return 'A';
+    case B:
+      return 'B';
+    case C:
+      return 'C';
+    case D:
+      return 'D';
+    case G:
+      return 'G';
+    default:
+      return '-';
+  }
 }
 
-std::vector<Edge> aStarSearch(const Graph &graph, const Vertex &start,
-                               const Vertex &goal) {
-  std::vector<bool> visited(graph.totalVertex(), false);
-  std::vector<Edge> tracePath;
-  Vertex curr = start;
-  visited[graph[start]] = true;
+struct Path {
+  std::deque<Vertex> path;
+  uint64_t cost;
+};
 
-  auto finished = [&visited]() {
-    return std::all_of(visited.begin(), visited.end(),
-                       [](bool value) { return value; });
-  };
+int  graph[VERTCOUNT][VERTCOUNT] =  {
+  { 0, 2, 3, 0, 0, 0 },
+  { 2, 0, 3, 2, 1, 0 },
+  { 3, 3, 0, 0, 4, 0 },
+  { 0, 2, 0, 0, 4, 3 },
+  { 0, 1, 4, 4, 0, 2 },
+  { 0, 0, 0, 3, 2, 0 }
+};
 
-  auto greedyFunction = [](const std::vector<Vertex> &vs) {
-    return *std::min_element(vs.begin(), vs.end(),
-                     [](const Vertex &lhs, const Vertex &rhs) ->bool {
-                       return heuristicCost[lhs] < heuristicCost[rhs];
-                     });
-  };
+std::deque<Vertex> edgesOf(Vertex vertex) {
+  int *list = graph[vertex];
+  std::deque<Vertex> result;
 
-  while (!finished()) {
-    auto neighbour = graph.neighbour(curr);
-    Vertex next = greedyFunction(neighbour);
-    VertexIndex nextIndex = graph[next];
-
-    std::vector<Edge> edges = graph.edgeStartVertex(curr);
-    Edge edge = *std::find_if(
-        edges.begin(), edges.end(),
-        [nextIndex](const Edge &edge) { return edge.End == nextIndex; });
-
-    tracePath.push_back(edge);
-
-    curr = next;
-    if (curr == goal) {
-      break;
-    }
+  for (uint32_t i = 0; i < VERTCOUNT; ++i) {
+    if (list[i])
+      result.push_back((Vertex)i);
   }
+  return result;
+}
 
-  return tracePath;
+Path bfs(Vertex start, Vertex end) {
+  std::deque<Path> possiblePaths { Path{{S}, 0} };
+  std::array<bool, VERTCOUNT> visited {};
+
+  Path currPath;
+  do {
+    currPath = std::move(possiblePaths.front());
+    possiblePaths.pop_front();
+    auto &endVertex = currPath.path.back();
+    if (visited[endVertex])
+      continue;
+    visited[endVertex] = true;
+
+    auto edges = edgesOf(endVertex);
+    std::sort(edges.begin(), edges.end(), [&endVertex](Vertex lhs, Vertex rhs){
+      return graph[endVertex][lhs] < graph[endVertex][rhs];
+    });
+
+    for (auto v : edges) {
+      if (visited[v])
+        continue;
+
+      Path newPath = currPath;
+      newPath.path.push_back(v);
+      newPath.cost += graph[endVertex][v];
+
+      possiblePaths.push_back(newPath);
+    }
+  } while(currPath.path.back() != G);
+
+  return currPath;
 }
 
 int main() {
-  Graph graph = initGraph();
-  auto tracePath = aStarSearch(graph, S, G);
-
-  displayPath(graph, tracePath, "Trace Path:");
-  uint64_t total = 0;
-  for (auto edge : tracePath) {
-    total += edge.Cost;
-  }
-  std::cout << "Path Cost: " << total << std::endl;
+  Path taken = bfs(S, G);
+  std::cout << "Actual Path\n";
+  for(auto curr = taken.path.begin(); curr != taken.path.end() - 1; ++curr) 
+    std::cout << VertexName(*curr) << " -> ";
+  std::cout << VertexName(*(taken.path.end() - 1)) << "\n";
+  std::cout << "Actual Path cost: " << taken.cost << std::endl;
 
   return EXIT_SUCCESS;
 }
